4abffa882a2077a9764309d0a1023562c110b0c3
Sooraj M S <soorajms4@gmail.com>
2025-04-12 10:45:37 +0530

    fix: corrected logical error in hash table

--- hash_table.py
+++ hash_table.py
@@ -2,27 +2,28 @@ class HashTable:
     def __init__(self):
         self.size = 10
         self.li = [[] for _ in range(self.size)]
-    
+
     def __get_hashed(self, key):
         index = 0
         for i in str(key):
             index += ord(i)
         return index % self.size
-    
+
     def __setitem__(self, key, val):
         index = self.__get_hashed(key)
-        if key in self.li[index]:
-            self.li[index][key] = val
-        else:
-            self.li[index].append([key, val])
-    
+        for i in self.li[index]:
+            if i[0] == key:
+                i[1] = val
+                return
+        self.li[index].append([key, val])
+
     def __getitem__(self, key):
         index = self.__get_hashed(key)
         for i in self.li[index]:
             if i[0] == key:
                 return i[1]
         return 'Not found'
-    
+
     def __delitem__(self, key):
         index = self.__get_hashed(key)
         for i in self.li[index]:
